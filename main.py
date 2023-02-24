6b963c60e0640b5d43235d121cdd2eb2e53e135d
LinkWhale <filipbjed@gmail.com>
2023-02-24 10:01:59 +0100

    still have errors

--- main.py
+++ main.py
@@ -72,12 +72,21 @@ def convert(var, v_type):
         print("spelt it wrong dumbass")
 
 
-def calculate_subnet(ip, subnet_mask, subnet):
+def calculate_subnet(ip, subnet_mask):
     current_step = 0
     while current_step < len(ip):
         subnet.append(ip[current_step] * subnet_mask[current_step])
         current_step += 1
+    return subnet
 
 
-ip = input("gib ip").split(".")
-ip_nonbin = calculate_nonbin(ip)
+u_ip = input("Input a valid IP: ")
+u_bin_ip = calculate_bin(u_ip.split("."))
+
+u_mask_bits = input("Input mask bits (1-32): ")
+u_subnet_mask_bin = []
+u_subnet_mask_bin = calculate_subnet_mask(u_mask_bits, u_subnet_mask_bin)
+u_subnet_mask = calculate_nonbin(u_subnet_mask_bin)
+u_subnet = calculate_subnet(u_bin_ip, u_subnet_mask_bin)
+
+print(f"IP: {u_ip}\nIP binary: {u_bin_ip}\nMask Bits: {u_mask_bits}\nSubnet mask binary: {u_subnet_mask_bin}\nSubnet mask: {u_subnet_mask}\nSubnet: {u_subnet}")
